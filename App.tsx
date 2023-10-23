19a227e78da66148c4449d31b349e489d12f510b
ali <ali412514n@gmail.com>
2023-10-23 09:36:57 +0430

    updated for expo str

--- App.tsx
+++ App.tsx
@@ -1,45 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { FlatList, ImageBackground, StyleSheet, Text, View } from 'react-native';
-import styles from './src/styles';
-import data from "./data.json"
+import { StatusBar } from "expo-status-bar";
+import {
+  FlatList,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import styles from "./styles";
+import data from "./data.json";
 
 type songType = {
-  id: number,
-  name: string,
-  url: string,
-}
+  id: number;
+  name: string;
+  url: string;
+};
 
 export default function App() {
   return (
-    <ImageBackground source={require("./src/assets/background.jpg")} style={styles.homeImgBackgroud}>
+    <ImageBackground
+      source={require("./assets/background.jpg")}
+      style={styles.homeImgBackgroud}
+    >
       <StatusBar style="auto" />
       <FlatList
         data={data.Songs}
         showsVerticalScrollIndicator={false}
         renderItem={({ item }: { item: songType }) => (
-          <View key={`song-${item.id}`} style={{
-            padding: 15,
-            marginVertical: 2,
-            marginHorizontal: 10,
-            borderRadius: 10,
-            backgroundColor: "rgba(255,255,255,0.25)",
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}>
+          <View
+            key={`song-${item.id}`}
+            style={{
+              padding: 15,
+              marginVertical: 2,
+              marginHorizontal: 10,
+              borderRadius: 10,
+              backgroundColor: "rgba(255,255,255,0.25)",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
             <Text style={styles.songName}>{item.name}</Text>
             <Text style={styles.songName}>{item.name}</Text>
           </View>
         )}
         ListFooterComponent={
           <View style={styles.playerContainer}>
-            <View style={{}}>
-
-            </View>
+            <View style={{}}></View>
           </View>
         }
       />
-
     </ImageBackground>
   );
 }
